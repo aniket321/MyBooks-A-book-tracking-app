c76095696ac9b2447660c27ba3de378a61d13e15
aniket.shashikant <aniket.shashikant@hashedin.com>
2020-05-15 16:03:17 +0530

    feat: Added ListBooks, BookShelf, SearchBooks components

---
+++ src/BookShelf.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class BookShelf extends Component {
+    render() {
+        return (
+            <div className='bookshelf'>
+                books
+            </div>
+        )
+    }
+}
+
+export default BookShelf
